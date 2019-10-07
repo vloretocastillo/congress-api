5b1ef997e7b3d477a65518a98c4d086e79a0343c
valentinaloreto <vloretocastillo@gmail.com>
2019-10-07 22:55:25 +0200

    added the states filter

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,89 @@ const changeText = (thisButton) => {
     isTextVisible ? readToggleButton.innerHTML = 'Read Less' : readToggleButton.innerHTML = 'Read More'
 }
 
+// ********************************************************* 
+
+
+const states = [
+    { name: 'ALABAMA', abbreviation: 'AL'},
+    { name: 'ALASKA', abbreviation: 'AK'},
+    { name: 'AMERICAN SAMOA', abbreviation: 'AS'},
+    { name: 'ARIZONA', abbreviation: 'AZ'},
+    { name: 'ARKANSAS', abbreviation: 'AR'},
+    { name: 'CALIFORNIA', abbreviation: 'CA'},
+    { name: 'COLORADO', abbreviation: 'CO'},
+    { name: 'CONNECTICUT', abbreviation: 'CT'},
+    { name: 'DELAWARE', abbreviation: 'DE'},
+    { name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC'},
+    { name: 'FEDERATED STATES OF MICRONESIA', abbreviation: 'FM'},
+    { name: 'FLORIDA', abbreviation: 'FL'},
+    { name: 'GEORGIA', abbreviation: 'GA'},
+    { name: 'GUAM', abbreviation: 'GU'},
+    { name: 'HAWAII', abbreviation: 'HI'},
+    { name: 'IDAHO', abbreviation: 'ID'},
+    { name: 'ILLINOIS', abbreviation: 'IL'},
+    { name: 'INDIANA', abbreviation: 'IN'},
+    { name: 'IOWA', abbreviation: 'IA'},
+    { name: 'KANSAS', abbreviation: 'KS'},
+    { name: 'KENTUCKY', abbreviation: 'KY'},
+    { name: 'LOUISIANA', abbreviation: 'LA'},
+    { name: 'MAINE', abbreviation: 'ME'},
+    { name: 'MARSHALL ISLANDS', abbreviation: 'MH'},
+    { name: 'MARYLAND', abbreviation: 'MD'},
+    { name: 'MASSACHUSETTS', abbreviation: 'MA'},
+    { name: 'MICHIGAN', abbreviation: 'MI'},
+    { name: 'MINNESOTA', abbreviation: 'MN'},
+    { name: 'MISSISSIPPI', abbreviation: 'MS'},
+    { name: 'MISSOURI', abbreviation: 'MO'},
+    { name: 'MONTANA', abbreviation: 'MT'},
+    { name: 'NEBRASKA', abbreviation: 'NE'},
+    { name: 'NEVADA', abbreviation: 'NV'},
+    { name: 'NEW HAMPSHIRE', abbreviation: 'NH'},
+    { name: 'NEW JERSEY', abbreviation: 'NJ'},
+    { name: 'NEW MEXICO', abbreviation: 'NM'},
+    { name: 'NEW YORK', abbreviation: 'NY'},
+    { name: 'NORTH CAROLINA', abbreviation: 'NC'},
+    { name: 'NORTH DAKOTA', abbreviation: 'ND'},
+    { name: 'NORTHERN MARIANA ISLANDS', abbreviation: 'MP'},
+    { name: 'OHIO', abbreviation: 'OH'},
+    { name: 'OKLAHOMA', abbreviation: 'OK'},
+    { name: 'OREGON', abbreviation: 'OR'},
+    { name: 'PALAU', abbreviation: 'PW'},
+    { name: 'PENNSYLVANIA', abbreviation: 'PA'},
+    { name: 'PUERTO RICO', abbreviation: 'PR'},
+    { name: 'RHODE ISLAND', abbreviation: 'RI'},
+    { name: 'SOUTH CAROLINA', abbreviation: 'SC'},
+    { name: 'SOUTH DAKOTA', abbreviation: 'SD'},
+    { name: 'TENNESSEE', abbreviation: 'TN'},
+    { name: 'TEXAS', abbreviation: 'TX'},
+    { name: 'UTAH', abbreviation: 'UT'},
+    { name: 'VERMONT', abbreviation: 'VT'},
+    { name: 'VIRGIN ISLANDS', abbreviation: 'VI'},
+    { name: 'VIRGINIA', abbreviation: 'VA'},
+    { name: 'WASHINGTON', abbreviation: 'WA'},
+    { name: 'WEST VIRGINIA', abbreviation: 'WV'},
+    { name: 'WISCONSIN', abbreviation: 'WI'},
+    { name: 'WYOMING', abbreviation: 'WY' }
+];
+
+let stateSelect = document.getElementById('state');
+
+for(let i = 0; i < states.length; i++) {
+    var option = document.createElement("option");
+    option.innerHTML = states[i].name;
+    option.value = states[i].abbreviation;
+    stateSelect.appendChild(option);
+}
+
 // ********************************************************* ADD EVENT LISTENERS  
 
 const checkboxesParties = document.getElementsByName('party') || null
+// const select = document.getElementById('state') || null
 if (checkboxesParties) {
     for (let i=0; i < checkboxesParties.length; i++) { checkboxesParties[i].addEventListener('change', () => filter() ) }
+}
+if (stateSelect) {
+    stateSelect.addEventListener('change', () => filter() ) 
 }
 
 // ********************************************************* GENERATE MEMBER TABLES FUNCTIONS 
@@ -44,6 +122,8 @@ const renderRow = (member) => {
     state = document.createElement('td')
     state.innerHTML = member.state 
     tr.appendChild(state)
+    // tr.setAttribute('data-state', member.state )
+
     
     seniority = document.createElement('td')
     seniority.innerHTML =  member.seniority 
@@ -52,7 +132,7 @@ const renderRow = (member) => {
     party = document.createElement('td')  
     party.innerHTML = member.party 
     tr.appendChild(party)
-    tr.setAttribute('data-party', member.party )
+    // tr.setAttribute('data-party', member.party )
 
     party_votes = document.createElement('td')
     party_votes.innerHTML =  member.votes_with_party_pct
@@ -72,10 +152,20 @@ const renderTable = (members) => {
 const filter = () => {
     let members = currentPage == "senate.html" ? dataSenate['results'][0]['members'] : currentPage == "representatives.html" ? dataHouse['results'][0]['members'] : null
     const checkboxesValuesParties = [...document.querySelectorAll('input[type=checkbox]:checked')].map(el => el.value)
+
+    const stateSelect = [...document.getElementById('state')].filter(el => el.selected)[0].value
+
+    if (stateSelect != "all") {
+        members = members.filter(el => el.state == stateSelect)
+    }
+
     members = members.filter(el => checkboxesValuesParties.indexOf(el.party) != -1)
     members = members.map(el => createMemberObj(el))
+
     document.getElementById('table-body').innerHTML = ''
     renderTable( members )
 }
+
+
 
 filter();
